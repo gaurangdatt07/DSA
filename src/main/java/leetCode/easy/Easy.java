f277babb6e3110804672f71767a10d42b62f3e60
NC23857_Gaurang <nc23857_gaurang@192.168.1.2>
2024-01-30 17:14:02 +0530

    75 leet code question

--- src/main/java/leetCode/easy/Easy.java
+++ src/main/java/leetCode/easy/Easy.java
@@ -1,6 +1,13 @@
 package leetCode.easy;
 
+import leetCode.ListNode;
+
 public interface Easy {
     //https://leetcode.com/problems/two-sum/
     int[] twoSum(int[] nums, int target);
+    int[] twoSumOptimised(int[] nums, int target);
+
+    //https://leetcode.com/problems/linked-list-cycle/
+     boolean hasCycle(ListNode head);
+     boolean hasCycleOptimised(ListNode head);
 }
--- src/main/java/leetCode/easy/EasyImpl.java
+++ src/main/java/leetCode/easy/EasyImpl.java
@@ -1,5 +1,12 @@
 package leetCode.easy;
 
+import leetCode.ListNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EasyImpl implements Easy{
     @Override
     public int[] twoSum(int[] nums, int target) {
@@ -11,5 +18,54 @@ public class EasyImpl implements Easy{
             }
         }
         return new int[]{};
+    }
+
+    @Override
+    public int[] twoSumOptimised(int[] nums, int target) {
+        Map<Integer,Integer>numMap=new HashMap<>();
+        for(int i = 0 ; i < nums.length;i++){
+            int diff = target-nums[i];
+            if(numMap.containsKey(diff)){
+                return new int[]{numMap.get(diff),i};
+            }
+            numMap.put(nums[i],i );
+        }
+        return new int[]{};
+    }
+
+
+    @Override
+    public boolean hasCycle(ListNode head) {
+        List<ListNode> nodeList=new ArrayList<>();
+        while(head!=null){
+            if(nodeList.contains(head)){
+                return true;
+            }
+            nodeList.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasCycleOptimised(ListNode head) {
+        // Floyd's Tortoise and Hare algorithm
+        if (head == null || head.next == null) {
+            return false;  // No cycle with less than 2 nodes
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;  // Reached end of the list, no cycle
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;  // Cycle detected
     }
 }
