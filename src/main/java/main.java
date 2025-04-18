e1021c668fef428992dacef81b8114fdf739f68f
gaurangdatt07 <gaurangdutt@ninjacart.com>
2025-04-18 22:47:46 +0530

    done all arrays and string and two pointers questions

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,10 @@
+import leetcode.medium._151_Reverse_Words_In_String;
+import leetcode.medium._443_String_Compression;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        _443_String_Compression object=new _443_String_Compression();
+        System.out.println(object.compress(new char[]{'a','a','b','c','c','c'}));
+
     }
 }
