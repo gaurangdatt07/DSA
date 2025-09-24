39c0627e6d114ffc4629c98a34b61b6ac2ef30fa
gdutt1 <gdutt@tekion.com>
2025-09-24 10:56:36 +0530

    Stack crwaler_log_folder baseballGame

---
+++ src/LinkedList/_1721_Swapping_Nodes_In_A_Linked_List.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+import utility.ListNode;
+
+/**
+ *url: https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+ */
+public class _1721_Swapping_Nodes_In_A_Linked_List {
+    public ListNode swapNodes(ListNode head, int k) {
+
+    }
+}
