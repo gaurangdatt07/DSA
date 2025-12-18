7db551724987eb2e62244f75a5693a23e846a76a
gdutt1 <gdutt@tekion.com>
2025-12-18 17:45:05 +0530

    restart

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
-import leetcode._441_ARRANGING_COINS;
-import leetcode._3_Longest_Substring_Without_Repeating_Characters;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        _3_Longest_Substring_Without_Repeating_Characters solution
-                = new _3_Longest_Substring_Without_Repeating_Characters();
-        _441_ARRANGING_COINS solution2=new _441_ARRANGING_COINS();
-        System.out.println(solution2.guessNumber(5));
+
     }
 }
